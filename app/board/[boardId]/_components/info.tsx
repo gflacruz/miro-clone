134b5b4f2e7c9c69d18948692c82de39d1e25d11
gflacruz <glacruz@zoho.com>
2024-05-08 19:48:11 -0400

    board info actions added

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,12 +1,80 @@
-export const Info = () => {
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { Hint } from "@/components/hint";
+import { useRenameModal } from "@/store/use-rename-modal";
+import { Actions } from "@/components/actions";
+import { Menu } from "lucide-react";
+
+interface InfoProps {
+  boardId: string;
+}
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+const TabSeparator = () => {
+  return <div className="text-neutral-300 px-1.5">|</div>;
+};
+export const Info = ({ boardId }: InfoProps) => {
+  const { onOpen } = useRenameModal();
+
+  const data = useQuery(api.board.get, {
+    id: boardId as Id<"boards">,
+  });
+
+  if (!data) return <InfoSkelton />;
+
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      Todo information about the bound
+      <Hint label="Go to boards" side="bottom" sideOffset={10}>
+        <Button asChild variant={"board"} className="px-2">
+          <Link href={"/"}>
+            <Image src="/logo.svg" alt="Board Logo" height={40} width={40} />
+            <span
+              className={cn(
+                "font-semibold text-xl ml-2 text-black",
+                font.className
+              )}
+            >
+              Board
+            </span>
+          </Link>
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Hint label="Edit title" side="bottom" sideOffset={10}>
+        <Button
+          variant={"board"}
+          onClick={() => onOpen(data._id, data.title)}
+          className="text-base font-normal px-2"
+        >
+          {data.title}
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Actions id={data._id} title={data.title} side="bottom" sideOffset={10}>
+        <div>
+          <Hint label="Main menu" side="bottom" sideOffset={10}>
+            <Button variant={"board"} size="icon">
+              <Menu />
+            </Button>
+          </Hint>
+        </div>
+      </Actions>
     </div>
   );
 };
 
-Info.Skeleton = function InfoSkelton() {
+export const InfoSkelton = () => {
   return (
     <div className="absolute w-[300px] top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md" />
   );
--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -6,7 +6,7 @@ export const Participants = () => {
   );
 };
 
-Participants.Skeleton = function ParticipantsSkelton() {
+export const ParticipantsSkelton = () => {
   return (
     <div className="absolute w-[100px] h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md" />
   );
